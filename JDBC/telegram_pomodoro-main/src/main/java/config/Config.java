379890d04f07e211966c60ccfd6866835e46172d
skriplex <alex-himki@mail.ru>
2026-03-20 08:59:10 +0300

    Рефакторинг

--- JDBC/telegram_pomodoro-main/src/main/java/config/Config.java
+++ JDBC/telegram_pomodoro-main/src/main/java/config/Config.java
@@ -3,16 +3,16 @@ package config;
 public record Config(String botApiToken, int workDuration, int restDuration, String dbUrl, String dbUser, String dbPassword) {
     public Config {
         if (botApiToken == null || botApiToken.isEmpty()) {
-            throw new RuntimeException("Токен не задан!");
+            throw new RuntimeException("TOKEN не задан!");
         }
         if (dbUrl == null || dbUrl.isEmpty()) {
-            throw new RuntimeException();
+            throw new RuntimeException("URL не задан!");
         }
         if (dbUser == null || dbUser.isEmpty()) {
-            throw new RuntimeException();
+            throw new RuntimeException("LOGIN не задан!");
         }
         if (dbPassword == null || dbPassword.isEmpty()) {
-            throw new RuntimeException();
+            throw new RuntimeException("PASSWORD не задан!");
         }
     }
 }
