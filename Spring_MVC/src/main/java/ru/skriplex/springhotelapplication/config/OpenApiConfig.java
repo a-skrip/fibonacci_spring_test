cd4865a642606f5fcaa2b359a0cfd0197c9274db
skriplex <alex-himki@mail.ru>
2026-04-03 12:14:42 +0300

    Доконфигурирован swagger

---
+++ Spring_MVC/src/main/java/ru/skriplex/springhotelapplication/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package ru.skriplex.springhotelapplication.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("HotelApp")
+                        .version("1.0.0")
+                        .description("Работа с карточками отелей")
+                        .contact(new Contact().email("skriplex@yandex.ru"))
+                );
+
+    }
+}
