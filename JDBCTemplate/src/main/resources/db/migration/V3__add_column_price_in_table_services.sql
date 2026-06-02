0871fba3f3005abe7f91cad2842e06f912129e77
skriplex <alex-himki@mail.ru>
2026-06-02 21:53:21 +0300

    добавлена миграция

---
+++ JDBCTemplate/src/main/resources/db/migration/V3__add_column_price_in_table_services.sql
@@ -0,0 +1,10 @@
+-- add column price
+ALTER TABLE services
+ADD COLUMN price INTEGER NOT NULL DEFAULT 0;
+
+-- set price
+UPDATE services SET price = 400 WHERE id = 'SOLARIUM';
+UPDATE services SET price = 200 WHERE id = 'POOL';
+UPDATE services SET price = 0 WHERE id = 'SAUNA';
+UPDATE services SET price = 1000 WHERE id = 'CRYOSAUNA';
+UPDATE services SET price = 500 WHERE id = 'CROSSFIT';
