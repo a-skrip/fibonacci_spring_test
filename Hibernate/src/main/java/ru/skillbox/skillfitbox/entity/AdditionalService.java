713062b7086ef0ed884f80a2115ea84b19d5c9e3
skriplex <alex-himki@mail.ru>
2026-06-03 16:53:11 +0300

    Проаннотированы сущности

--- Hibernate/src/main/java/ru/skillbox/skillfitbox/entity/AdditionalService.java
+++ Hibernate/src/main/java/ru/skillbox/skillfitbox/entity/AdditionalService.java
@@ -1,9 +1,12 @@
 package ru.skillbox.skillfitbox.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,12 +15,26 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "services")
 public class AdditionalService {
-    
+
+    @Id
+    @Column(name = "id", nullable = false)
     private String id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @CreationTimestamp  // автоматически проставляет дату при вставке
+    @Column(name = "created_datetime", updatable = false)
     private LocalDateTime createdDatetime;
+
+    @UpdateTimestamp  // автоматически обновляется при изменении
+    @Column(name = "updated_datetime")
     private LocalDateTime updatedDatetime;
 
-    private List<Client> clients;
+
+    @ManyToMany(mappedBy = "services", fetch = FetchType.LAZY)  // ← mappedBy!
+    private List<Client> clients /*= new ArrayList<>()*/;
 }
--- Hibernate/src/main/java/ru/skillbox/skillfitbox/entity/Client.java
+++ Hibernate/src/main/java/ru/skillbox/skillfitbox/entity/Client.java
@@ -1,9 +1,9 @@
 package ru.skillbox.skillfitbox.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,19 +14,55 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "clients")
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "surname", nullable = false)
     private String surname;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "patronymic")
     private String patronymic;
+
+    @Column(name = "birthday", nullable = false)
     private LocalDate birthday;
+
+    @Column(name = "phone", nullable = false)
     private String phone;
+
+    @Column(name = "email", nullable = false)
     private String email;
+
+    @Column(name = "is_active", nullable = false)
     private Boolean isActive;
+
+    @CreationTimestamp  // автоматически проставляет дату при вставке
+    @Column(name = "created_datetime", updatable = false)
     private LocalDateTime createdDatetime;
+
+    @UpdateTimestamp  // автоматически обновляется при изменении
+    @Column(name = "updated_datetime")
     private LocalDateTime updatedDatetime;
 
-    private Trainer trainer;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "locker_id", unique = true)
     private Locker locker;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trainer_id")
+    private Trainer trainer;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "client_services",
+            joinColumns = @JoinColumn(name = "client_id"),
+            inverseJoinColumns = @JoinColumn(name = "service_id")
+    )
     private List<AdditionalService> services;
 }
