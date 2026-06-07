51997b8fa852118b16477427c0edfe92d1206dbc
skriplex <alex-himki@mail.ru>
2026-06-08 00:00:48 +0300

    написаны тесты проверки репозитория FibonacciController

---
+++ Spring_test/src/test/java/com/skillbox/fibonacci/adapter/web/FibonacciControllerTest.java
@@ -0,0 +1,67 @@
+package com.skillbox.fibonacci.adapter.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigInteger;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+class FibonacciControllerTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    @DisplayName("При запросе индекса > 1 -> 200.OK и JSON Тела")
+    void whenIndexIsValid_thenReturnOkAndFibonacciNumber() {
+        int index = 11;
+        String url = "http://localhost:" + port + "/fibonacci/" + index;
+        ResponseEntity<FibonacciResponse> response =
+                restTemplate.getForEntity(url, FibonacciResponse.class);
+
+        BigInteger value = BigInteger.valueOf(89);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().index()).isEqualTo(11);
+        assertThat(response.getBody().value()).isEqualTo(value);
+
+    }
+
+    @Test
+    @DisplayName("При запросе индекса < 1 -> 400 и текст ошибки")
+    void sendIndexLessThanOneAndGet400AndErrorText () {
+        int invalidIndex = -1;
+
+        String url = "http://localhost:" + port + "/fibonacci/" + invalidIndex;
+        ResponseEntity<String> response =
+                restTemplate.getForEntity(url, String.class);
+
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody()).contains("Число Фибоначчи не может иметь отрицательный или нулевой индекс");
+        assertThat(response.getBody()).contains(String.valueOf(invalidIndex));
+    }
+
+}
